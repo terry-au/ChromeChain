2a2583b733a999c967371d6c7225d7cd3c2f5ae8
terry1994 <terry1994dev@gmail.com>
2017-03-20 19:52:14 +1100

    Project staging. Update .gitignore.

---
+++ ChromeChain/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ChromeChain
+//
+//  Created by Terry Lewis on 20/3/17.
+//  Copyright (c) 2017 terry1994. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+
+    return 0;
+}
