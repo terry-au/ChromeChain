0706b4610a6174d8c8f2a3aaddcaec21c7f22b12
terry1994 <terry1994dev@gmail.com>
2017-03-20 19:58:16 +1100

    Add ChromeChain library. Add library to console application.

---
+++ ChromeChain/ChromeChain.m
@@ -0,0 +1,13 @@
+//
+//  ChromeChain.m
+//  ChromeChain
+//
+//  Created by Terry Lewis on 20/3/17.
+//  Copyright © 2017 terry1994. All rights reserved.
+//
+
+#import "ChromeChain.h"
+
+@implementation ChromeChain
+
+@end
--- ChromeChain/ChromeChainConsole/main.m
+++ ChromeChain/ChromeChainConsole/main.m
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "ChromeChain.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
